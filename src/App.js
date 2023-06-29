5e8f74a81e221751510b0590b665fc2070b10ed7
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-06-29 10:57:34 +0100

    all done except timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
 
 function App() {
+
+  const fizzfunc = (inputNumber) => {
+    if (inputNumber %3 === 0 && inputNumber %5 ===0){
+      return ("fizzbuzz");
+    } else if (inputNumber % 3 === 0){
+      return ('fizz');
+    } else if (inputNumber % 5 === 0){
+      return ("buzz")
+    }else{
+      return(inputNumber);
+    }
+  }
+
+  const colors = ['white', 'red', 'orange', 'green', 'purple', 'blue', 'brown', 'lime'];
+
+  const [color, setColor] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
+  const[fizzbuzz, setFizzbuzz] = useState(number);
+  const[result, setResult] = useState("")
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(fizzbuzz == text.toLowerCase()){
+      setResult("CORRECT")
+    }else{
+      setResult("FALSE");
+    }
+  }
+
+  useEffect(() => {
+    setFizzbuzz(fizzfunc(number));
+  }, [number])
+
+  const handleIncrementButton = () => {
+    setNumber(number + 1);
+  }
+
+  const handleColorButton = () => {
+    if(color === 7){
+    setColor(0)} else{
+      setColor(color + 1);
+    };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className={colors[color]}>FizzBuzz</h1>
+      <button value={number} onClick={handleIncrementButton}>+</button>
+      <button value={color} onClick={handleColorButton}>Switch Colors</button>
+      <h3>{number}</h3>
+      {/* <h3>{fizzbuzz}</h3> */}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleTextChange}/>
+        <input type="submit" value="Post"/>
+      </form>
+      <h1>{result}</h1>
+    </>
   );
 }
 
